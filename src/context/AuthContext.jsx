e2530a29b23c2f1085a9f9775581cea214dc906e
thomas78125 <thomas78125@gmail.com>
2023-01-27 11:11:17 +0100

    AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { createContext, useState } from 'react';
 
-export default function AuthContext() {
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const login = (user) => {
+    setIsAuthenticated(true);
+    setUser(user);
+  }
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    setUser(null);
+  }
+
+  const context = {
+    isAuthenticated,
+    user,
+    login,
+    logout
+  };
+
   return (
-    <div>AuthContext</div>
+    <AuthContext.Provider value={context}>
+      {children}
+    </AuthContext.Provider>
   )
 }
+
+export default AuthContext;
