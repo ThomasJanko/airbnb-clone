b539198c34c100cb8236a228dd689912c33e1d8d
thomas78125 <thomas78125@gmail.com>
2023-02-10 12:51:57 +0100

    LoginForm style

--- src/pages/auth/login/index.js
+++ src/pages/auth/login/index.js
@@ -33,7 +33,7 @@ const Index = () => {
                   // setUser(res.data.user)
                    localStorage.setItem('Auth', JSON.stringify(res.data))
                 //    login();
-                //    router.push('/users');
+                   router.push('/');
             }
             else{
                 setMessage('Email or password incorrect !')
@@ -54,25 +54,25 @@ const Index = () => {
     }
   return (
     <div className='mx-auto  '>
-        <h2 className='title font-bold mt-32 text-center text-2xl'>LoginForm</h2>
-        <div className='bg-green-100 mx-auto border rounded-2xl mt-4 card shadow-md elevation-2' style={{width: '40%', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)'}}>
+        <h2 className='title font-bold mt-32 text-center text-primary text-2xl'>Connexion</h2>
+        <div className='bg-transparent mx-auto border rounded-2xl mt-4 card shadow-md elevation-2' style={{width: '40%', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)'}}>
          <div className='mx-8 mt-4 flex-col '>
-            <label className='block text-gray-700 text-sm font-bold mt-2' htmlFor="email">EMAIL :</label><br/>
+            <label className='block text-secondary text-sm font-bold mt-2' htmlFor="email">EMAIL :</label><br/>
             <input className='shadow appearance-none border rounded w-full p-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' value={email} type="email" onChange={(e) => setEmail(e.target.value)} /><br/>
             
-            <label className='block text-gray-700 text-sm font-bold mt-2' htmlFor="password">PASSWORD :</label><br/>
+            <label className='block text-secondary text-sm font-bold mt-2' htmlFor="password">PASSWORD :</label><br/>
             <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' value={password} type="password" onKeyDown={(e) => e.key==='Enter' && handleForm()}  onChange={(e) => setPasssword(e.target.value)} /><br/>
 
             <div className='flex justify-center'>
-                <button className='mt-4 text-center mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' onClick={() => handleForm()}> Login </button>
+                <button className='mt-4 text-center mx-auto bg-primary hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' onClick={() => handleForm()}> Login </button>
             </div>
 
             <div className='flex justify-between'>
-                    <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 pb-2" href="/auth/register">
+                    <a className="inline-block align-baseline font-bold text-sm text-primary cursor-pointer hover:text-red-700 pb-2" href="/auth/register">
                         Register
                     </a>
                 
-                <a className="inline-block align-baseline font-bold text-sm text-blue-500  hover:text-blue-800 pb-2">
+                <a className="inline-block align-baseline font-bold text-sm text-primary cursor-pointer hover:text-red-700 pb-2">
                     Forgot Password?
                 </a>
             </div>
