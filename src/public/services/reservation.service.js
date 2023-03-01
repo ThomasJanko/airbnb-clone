bfae9c8dee1131806dcbdff7a7a9529360d383b2
thomas78125 <thomas78125@gmail.com>
2023-03-01 08:46:02 +0100

    t

---
+++ src/public/services/reservation.service.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export default{
+    
+    addReservation(form, jwt){
+        const config = {
+            headers: {
+              authorization: jwt,
+            },
+          };
+       return axios.post(`${URL}/reservation/reservation`, form, config)
+        .then(res=>res)
+        .catch(err=>err)
+    },
+
+   
+     
+}
